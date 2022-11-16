633f0553457afac5976e646b244455696cb1cb01
Glazkova Nataliya <nataliya.petrovnaa@yandex.ru>
2022-11-16 20:35:20 +0300

    Sozdanproekt

---
+++ TableCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableCell.swift
+//  iTime
+//
+//  Created by Nataliya Glazkova on 14.11.2022.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    @IBOutlet weak var tableLabel: UILabel!
+
+}
